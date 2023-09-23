d6fcf731f8d3db765ecaf7228de52cccd6b5b89c
DeLucca990 <pedruca2001@gmail.com>
2023-09-23 11:12:10 -0300

    Projeto 5

---
+++ Projeto5/client.py
@@ -0,0 +1,242 @@
+#####################################################
+# Camada Física da Computação
+#Carareto
+#11/08/2022
+#Aplicação
+####################################################
+
+
+#esta é a camada superior, de aplicação do seu software de comunicação serial UART.
+#para acompanhar a execução e identificar erros, construa prints ao longo do código! 
+
+
+from enlace import *
+import time
+import numpy as np
+import os
+import math
+from datetime import datetime
+from crccheck.crc import Crc16
+
+# voce deverá descomentar e configurar a porta com através da qual ira fazer comunicaçao
+#   para saber a sua porta, execute no terminal :
+#   python -m serial.tools.list_ports
+# se estiver usando windows, o gerenciador de dispositivos informa a porta
+
+#use uma das 3 opcoes para atribuir à variável a porta usada
+#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
+com1 = enlace('COM3')
+
+class Client:
+    def __init__(self, file, serialName):
+        self.serialName = serialName
+        self.cancel_reason = None
+        self.head = None
+        self.file = file
+        self.eop = b'\xAA\xBB\xCC\xDD'
+        self.payloads = 0
+        self.h0 = 0 # Tipo da Mensagem
+        self.h1 = b'\x00' # Se tipo for 1: número do servidor. Qualquer outro tipo: livre 
+        self.h2 = b'\x00' # Livre
+        self.h3 = 0 # Numero total de pacotes
+        self.h4 = 0 # Numero do pacote enviado
+        self.h5 = 0 # Se tipo for handshake: id do arquivo (crie um para cada arquivo). Se tipo for dados: tamanho do payload.
+        self.h6 = b'\x00' # Pacote solicitado para recomeço quando a erro no envio.
+        self.h7 = 0 # Último pacote recebido com sucesso.
+        self.h8 = b'\x00' # CRC 
+        self.h9 = b'\x00' # CRC 
+        self.logs = ''
+
+    def startClient(self):
+        self.clientCom = enlace(self.serialName)
+        self.clientCom.enable()
+
+    def closeClient(self):
+        print("-------------------------")
+        print("Comunicação encerrada")
+        print("-------------------------")
+        self.clientCom.disable()
+        exit()
+    
+    def sendSacrifice(self):
+        self.clientCom.sendData(b'\x00')
+        time.sleep(0.1)
+        print("Enviando byte de sacrifício")
+        
+    # Cria os payloads
+    def createPayloads(self):
+        size = 114
+        self.payloads = []
+        for i in range(0, len(self.file), size):
+            self.payloads.append(self.file[i:i + size]) # Divide os pacotes em tamanhos iguais a 114 bytes
+        return self.payloads
+
+    def createCRC(self):
+        data = self.payloads[int.from_bytes(self.h4,"big")-1]
+        # if int.from_bytes(self.h4,"big") - 1 == 2:
+        #     data = self.payloads[1]
+        # else:
+        crc1, crc2 = Crc16.calc(data).to_bytes(2, byteorder="big")
+        self.h8 = crc1.to_bytes(1, byteorder="big")
+        self.h9 = crc2.to_bytes(1, byteorder="big")
+
+    # Define o tipo da mensagem
+    def TypeMsg(self, n):
+        self.h0 = (n).to_bytes(1, byteorder="big")
+        # Mensagem do tipo Handshake
+        if n == 1:
+            self.h5 = b'\x00'
+            #self.h3 = self.numPack()
+        # Mensagem do tipo dados
+        elif n == 3:
+            self.h5 = len(self.payloads[int.from_bytes(self.h4,"big")-1]) # -1 pois o index começa em 0
+            self.h5 = (self.h5).to_bytes(1, byteorder="big")
+
+    # Define a mensagem
+    def numMsg(self, n):
+        self.h4 = (n).to_bytes(1, byteorder="big")  # numero pacote
+        self.h7 = (n-1).to_bytes(1, byteorder="big")    # numero ultimo pacote
+    
+    # Define o número total de pacotes
+    def numPack(self):
+        lenPayload = len(self.file)
+        h3 = math.ceil(lenPayload/114)
+        self.h3 = (h3).to_bytes(1, byteorder="big")
+
+    # Constrói o head
+    def buildHead(self):
+        self.head = self.h0 + self.h1 + self.h2 + self.h3 + self.h4 + self.h5 + self.h6 + self.h7 + self.h8 + self.h9
+    
+    # Constrói o pacote
+    def buildDatagram(self):
+        return self.head + self.payloads[int.from_bytes(self.h4,"big")-1] + self.eop
+    
+    # Checa o tempo máximo para a resposta do servidor
+    def SendWait(self, pacote):
+        timeMax = time.time()
+        while True: 
+            self.clientCom.sendData(pacote)
+            time.sleep(.1)
+            self.createLog(pacote, 'envio')
+            time.sleep(.5)
+            len_conf = self.clientCom.rx.getBufferLen()
+            time.sleep(.5)
+            if len_conf != 0:
+                confirmacao, lenConfimacao = self.clientCom.getData(15)
+                if type(confirmacao) == str:
+                    print(confirmacao)
+                else:
+                    return confirmacao
+            else:
+                timeF = time.time()
+                if timeF - timeMax > 5:
+                    self.cancel_reason = "Timeout"
+                    print("\033[31mServidor não respondeu. Cancelando comunicação.\033[0m")
+                    self.createLog(pacote, 'envio')
+                    break
+
+    # Handshake
+    def handshake(self):
+        payload = b'\x00'
+        self.TypeMsg(1)
+        self.h3 = b'\x00'
+        self.h4 = b'\x00'
+        self.h7 = b'\x00'
+        self.buildHead()
+        datagram = self.head + payload + self.eop
+        return self.SendWait(datagram)
+    
+    # Checa o tipo de mensagem na confirmação enviada pelo servidor
+    def checkTypeMsg(self, confirmacao):
+        if confirmacao[0] == 4: # 4 indica sucesso (confirmacao[0] = h0)
+            self.createLog(confirmacao, 'receb')
+            print(f"Número do pacote: {confirmacao[7]}")
+            print("\033[32mTudo certo! O servidor recebeu o pacote corretamente.\033[0m")
+        else:
+            self.createLog(confirmacao, 'receb')
+            numPacoteCorreto = confirmacao[7]
+            print(f"\033[33mAlgo deu errado no envio. Precisamos reenviar o pacote {numPacoteCorreto}\033[0m")
+            return numPacoteCorreto
+    
+    # Escreve os logs
+    def createLog(self, data, tipo):
+        tempo = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        tipoMsg = data[0]
+        tamDatagram = len(data)
+        numPacoteEnviado = data[4]
+        totalPacotes = data[3]
+        self.logs += f"{tempo} / {tipo} / {tipoMsg} / {tamDatagram} / {numPacoteEnviado} / {totalPacotes}\n"
+
+        if self.cancel_reason is not None:
+            self.logs += f"{tempo} / {tipo} / 5 / {tamDatagram} / {numPacoteEnviado} / {totalPacotes} / {self.cancel_reason}\n"
+        
+    def writeLog(self):
+        with open(f'Projeto4/Logs/logClient3.txt', 'w') as file:
+            file.write(self.logs)
+
+serialName = "COM3"
+path = 'Projeto4/Images/imgTx.jpg'
+file = open(path, "rb").read()
+
+def main():
+    try:
+        client = Client(file, "COM3")
+        client.startClient()
+
+        # Envia byte de sacrifício
+        client.sendSacrifice()
+
+        # Handshake
+        print("Enviando Handshake\n")
+        if client.handshake() is None:
+            client.closeClient()
+        else:
+            print("Handshake realizado com sucesso")
+        
+        # Envia os pacotes
+        print("Iniciando envio de pacotes")
+        client.createPayloads()
+        client.numPack()
+        # h3 = número total de pacotes
+        # h4 = número do pacote sendo enviado
+        h4 = 1
+
+        c = 0
+        while h4 < int.from_bytes(client.h3, "big"):
+            print(f"Enviando informações referentes ao pacote {h4}")
+            client.numMsg(h4) # Define o número do pacote
+            client.createCRC() # Cria o protocolo CRC
+            print(f"CRC: {client.h8} {client.h9}")
+            client.TypeMsg(3) # 3 = dados
+            client.buildHead()
+            pacote = client.buildDatagram()
+            confirmacao = client.SendWait(pacote) # Envia o pacote e espera a confirmação do servidor
+
+            if confirmacao is None: # Se não houver resposta do servidor, encerra a comunicação
+                client.writeLog()
+                client.closeClient()
+            else:
+                numPacoteCorreto = client.checkTypeMsg(confirmacao)
+                if numPacoteCorreto is None: # Se o número do pacote estiver errado
+                    if h4 == 7: # Se o número do pacote for 7, pula para o 9 (FORÇA O ERRO)
+                        h4 += 1
+                    else:
+                        h4 += 1
+                    c += 1
+                else: # Se o número do pacote estiver correto, envia o próximo
+                    h4 = numPacoteCorreto
+                    c = numPacoteCorreto - 1 
+
+        client.writeLog()
+        client.closeClient()
+        
+    except Exception as erro:
+        print("ops! :-\\")
+        print(erro)
+        com1.disable()
+        
+
+    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+if __name__ == "__main__":
+    main()
---
+++ Projeto5/server.py
@@ -0,0 +1,205 @@
+#####################################################
+# Camada Física da Computação
+#Carareto
+#11/08/2022
+#Aplicação
+####################################################
+
+
+#esta é a camada superior, de aplicação do seu software de comunicação serial UART.
+#para acompanhar a execução e identificar erros, construa prints ao longo do código! 
+
+import math
+from enlace import *
+import time
+import numpy as np
+import os
+from datetime import datetime
+from crccheck.crc import Crc16
+
+# voce deverá descomentar e configurar a porta com através da qual ira fazer comunicaçao
+#   para saber a sua porta, execute no terminal :
+#   python -m serial.tools.list_ports
+# se estiver usando windows, o gerenciador de dispositivos informa a porta
+
+#use uma das 3 opcoes para atribuir à variável a porta usada
+#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
+#serialName = "COM3"                  # Windows(variacao de)
+
+class Server:
+    def __init__(self, serialName):
+        self.serialName = serialName
+        self.logs = ''
+        self.cancel_reason = None
+
+    def startServer(self):
+        self.serverCom = enlace(self.serialName)
+        self.serverCom.enable()
+    
+    def closeServer(self):
+        print("-------------------------")
+        print("Comunicação encerrada")
+        print("-------------------------")
+        self.serverCom.disable()
+        exit()
+    
+    def receiveData(self, n):
+        data = self.serverCom.getData(n)
+        return data
+
+    def sendData(self, data):
+        self.serverCom.sendData(data)
+
+    # Recebe o handshake do client
+    def receiveHandshake(self, n):
+        print('entrei pra receber')
+        pack, lenpack = self.serverCom.getData(n)
+        time.sleep(.1)
+        self.createLog(pack, 'receb') # Cria o log
+        pack = list(pack)
+        pack = list(map(int, pack)) # Transforma os dados em inteiros
+        pack[0] = 2 # Muda o tipo da mensagem para 2 (handshake)
+        responseHandshake = b''
+        for i in pack:
+            responseHandshake += (i).to_bytes(1, byteorder='big')    
+    
+        return responseHandshake, lenpack
+    
+    def splitHead(self, data):
+        head = data[:10]
+        h0 = head[0] # Tipo da Mensagem
+        h1 = head[1] # Se tipo for 1: número do servidor. Qualquer outro tipo: livre 
+        h2 = head[2] # Livre
+        h3 = head[3] # Numero total de pacotes
+        h4 = head[4] # Numero do pacote enviado
+        h5 = head[5] # Se tipo for handshake: id do arquivo (crie um para cada arquivo). Se tipo for dados: tamanho do payload.
+        h6 = head[6] # Pacote solicitado para recomeço quando a erro no envio.
+        h7 = head[7] # Último pacote recebido com sucesso.
+        h8 = head[8] # CRC
+        h9 = head[9] # CRC
+        return h0, h1, h2, h3, h4, h5, h6, h7, h8, h9
+    
+    def checkMsgIntegrity(self, pacote, numPacote):
+        self.createLog(pacote, 'receb')
+        h0, h1, h2, h3, h4, h5, h6, h7, h8, h9 = self.splitHead(pacote)
+        # Checando se o número do pacote enviado está correto
+        if h4 != numPacote:
+            print(f"\033[33mO número do pacote está errado! Por favor reenvie o pacote {numPacote}\033[0m")
+            h0 = 6 # 6 indica erro
+            h7 = numPacote
+            confirmacao = [h0, h1, h2, h3, h4, h5, h6, h7, h8, h9]
+            responseCorrectMsg = b'' # Cria a mensagem de confirmação em bytes para enviar ao client
+            for i in confirmacao: # Transforma cada elemento da lista em bytes
+                i = (i).to_bytes(1, byteorder="big")
+                responseCorrectMsg += i
+            self.serverCom.sendData(responseCorrectMsg + b'\x00' + b'\xAA\xBB\xCC\xDD')
+            #self.createLog(responseCorrectMsg + b'\x00' + b'\xAA \xBB \xCC \xDD', 'envio')
+            time.sleep(0.5)
+            return h4, h3
+
+        # Checando se o EOP está no local correto
+        eop = pacote[len(pacote)-4:len(pacote)+1]
+        if eop != b'\xAA\xBB\xCC\xDD':
+            print(f"\033[33mO eop está no local errado! Por favor reenvie o pacote {numPacote}\033[0m")
+            return h4, h3
+            
+        # Se tudo estiver certo, deu bom
+        else:
+            # Checa CRC
+            crc1, crc2 = Crc16.calc(pacote[10:len(pacote)-4]).to_bytes(2, byteorder="big")
+            crc_h8 = crc1.to_bytes(1, byteorder="big")
+            crc_h9 = crc2.to_bytes(1, byteorder="big")
+            if (crc_h8 != h8.to_bytes(1, byteorder = "big")) or (crc_h9 != h9.to_bytes(1, byteorder = "big")):
+                print(f"\033[36mO CRC está errado! Por favor reenvie o pacote {numPacote}\033[0m")
+                return h4, h3
+            else:
+                print("\033[32mEstá tudo certo com a mensagem! Vamos enviar a confirmação.\033[0m")
+                h0 = 4 # 4 indica sucesso
+                h7 = numPacote
+                confirmacao = [h0, h1, h2, h3, h4, h5, h6, h7, h8, h9]
+                responseCorrectMsg = b''
+                for i in confirmacao:
+                    i = (i).to_bytes(1, byteorder="big")
+                    responseCorrectMsg += i
+                self.serverCom.sendData(responseCorrectMsg + b'\x00' + b'\xAA\xBB\xCC\xDD')
+                self.createLog(responseCorrectMsg + b'\x00' + b'\xAA\xBB\xCC\xDD', 'envio')
+                time.sleep(0.5)
+                return h4, h3
+
+    # Escreve os logs
+    def createLog(self, data, tipo):
+        tempo = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        tipoMsg = data[0]
+        tamMsg = len(data)
+        numPacoteEnviado = data[4]
+        totalPacotes = data[3]
+        self.logs += f"{tempo} / {tipo} / {tipoMsg} / {tamMsg} / {numPacoteEnviado} / {totalPacotes}\n"
+
+        if self.cancel_reason is not None:
+            self.logs += f"{tempo} / {tipo} / 5 / {tamMsg} / {numPacoteEnviado} / {totalPacotes} / {self.cancel_reason} \n"
+ 
+    def writeLog(self):
+        with open(f'Projeto4/Logs/logServer3.txt', 'w') as file:
+            file.write(self.logs)
+
+    def receiveSacrifice(self):
+        #Byte de sacrifício
+        print("Esperando 1 byte de sacrifício")
+        rxBuffer, nRx = self.receiveData(1)
+        self.serverCom.rx.clearBuffer()
+        time.sleep(.1)
+        
+serialName = "COM3"
+
+def main():
+    try:
+        server = Server('COM3')
+        server.startServer()
+
+        server.receiveSacrifice()
+
+        # HandShake
+        print("Esperando o Handshake do Client...\n")
+        
+        pack, lenpack = server.receiveHandshake(15)
+        print("\033[32mHandshake recebido com sucesso, enviando resposta\033[0m")
+        server.sendData(pack)
+        time.sleep(0.5)
+
+        # Recebendo o pacote
+        data = b'' # Dados que serão armanezados nessa variável
+        numPack = 1 # Número do pacote que está sendo recebido
+        
+        while True:
+            print(f"Recebendo informações do pacote {numPack}")
+            head, lenhead = server.receiveData(10)
+            len_payload = head[5] # Definimos essa variável (h3) para saber o tamanho do payload
+            payloadEOP, lenpayloadEOP = server.receiveData(len_payload + 4)
+            numPackReceived, numPackTotal = server.checkMsgIntegrity(head + payloadEOP, numPack)
+            if numPackReceived == numPack:
+                data += payloadEOP[0:lenpayloadEOP - 4]
+                numPack += 1 # Caso o pacote esteja correto, passamos para o próximo
+            if numPackReceived == numPackTotal -1: # Se todos os pacotes foram recebidos, saímos do loop
+                data += payloadEOP[0:lenpayloadEOP - 4]
+                break
+        
+        # Escrevendo o arquivo
+        print("Escrevendo o arquivo")
+        path = "Projeto4/Images/imgRx.jpg"
+        with open(path, 'wb') as file:
+            file.write(data)
+        file.close()
+        print("Arquivo escrito com sucesso!")
+
+        server.writeLog()
+        server.closeServer()
+
+    except Exception as erro:
+        print("ops! :-\\")
+        print(erro)
+        server.closeServer()
+        
+    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+if __name__ == "__main__":
+    main()
