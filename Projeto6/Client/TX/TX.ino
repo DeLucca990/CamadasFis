8ce771196180852f65f9f0d2e9bc5a071073cda2
DeLucca990 <pedruca2001@gmail.com>
2023-10-20 17:41:10 -0300

    Project6 done

--- Projeto6/Client/TX/TX.ino
+++ Projeto6/Client/TX/TX.ino
@@ -1,54 +1,44 @@
 #include <Arduino.h>
 
-int pinClient = 5; 
-char message[] = "Camadas Fisicas";
+int pinClient = 7;
+byte msg = 0x0A; // letra A em hexadecimal
+int msgBinary = int(msg);
 float baudrate = 9600;
 
 void setup() {
   pinMode(pinClient, OUTPUT);
   Serial.begin(baudrate); 
-  digitalWrite(pinClient, HIGH);
+  digitalWrite(pinClient, HIGH); 
 }
 
-float timeSkipper(float skipTime=1, float baudrate=9600, float T0=0) { // Função para criar os atrasos necessários
-  double clock = 1 / (21 * pow(10, 6)); // Período do clock
-  double T = 1 / baudrate; //Período de 1 bit (quanto tempo levará para ser transmitido)
-  int numberOfClocks = ceil(T / clock) + 1; // Número de ciclos do clock
-  for (int i = 0; i < int(numberOfClocks * skipTime); i++) {
-    asm("NOP"); // Cria o atraso necessário
-  }
+float timeSkipper(float skipTime=1, float baudrate=9600, float T0=0){
+  double clock = 1 / (21 * pow(10, 6)); // tempo de 1 pulso do relógio (T = 1/frequência)
+  double T = 1 / baudrate; // tempo entre cada pulso do relógio, em segundos
+  int numberOfClocks = ceil(T / clock) + 1; // número de pulsos do relógio a esperar, arredondado para o inteiro mais próximo
+  for (int i = 0; i < int(numberOfClocks * skipTime); i++){ asm("NOP"); } // aguarde o tempo especificado
 }
 
 void loop() {
-  for (int i = 0; i < strlen(message); i++) {
-    char currentChar = message[i];
+  int amountOf1s = 0; // contador para a quantidade de uns na mensagem
 
-    digitalWrite(pinClient, 0); // Pino LOW (indica início de transmissão)
-    timeSkipper(); // Essa função sempre será chamada para criar o atraso
-
-    for (int j = 0; j < 8; j++) {
-      int currentBit = 1 & (currentChar >> j); 
-      digitalWrite(pinClient, currentBit);
-      timeSkipper();
-    }
-
-    int amountOf1s = 0; // Conta a quantidade de bits 1 no caractere
-    for (int j = 0; j < 8; j++) {
-      int currentBit = 1 & (currentChar >> j);
-      if (currentBit == 1) {
-        amountOf1s++;
-      }
-    }
-
-    int parityBit = amountOf1s % 2; // Calcula a paridade: Se for ímpar é 1, se não é 0
-    digitalWrite(pinClient, parityBit); 
-    timeSkipper(); 
-
-    digitalWrite(pinClient, 1); // Pino HIGH (Indica Final da transmissão)
-    timeSkipper();
-
-    Serial.print("Enviando o Caractere: ");
-    Serial.println(currentChar);
-    delay(1500);
+  digitalWrite(pinClient, 0); // envie o bit de início
+  timeSkipper(); // sempre chamado após o envio de um bit
+  
+  for (int i = 0; i < 8; i++){ // envie os 8 bits (1 byte) da mensagem
+    int currentBit = 1 & (msgBinary >> i); // obtenha o bit atual da mensagem, começando pelo bit menos significativo
+    digitalWrite(pinClient, currentBit); // envie o bit atual
+    if (currentBit == 1){ amountOf1s++; } // conte a quantidade de uns na mensagem
+    timeSkipper(); // sempre chamado após o envio de um bit
   }
+
+  int parityBit = amountOf1s % 2; // se o resto for igual a 0, então é par, senão é ímpar
+  digitalWrite(pinClient, parityBit); // envie o bit de paridade
+  timeSkipper(); // sempre chamado após o envio de um bit
+
+  digitalWrite(pinClient, 1); // envie o bit de parada
+  timeSkipper(); // sempre chamado após o envio de um bit
+  
+  Serial.print("Caractere Enviado: ");
+  Serial.println(msg, HEX); // imprima a mensagem enviada (em hexadecimal)
+  delay(1500);
 }
--- Projeto6/Server/RX/RX.ino
+++ Projeto6/Server/RX/RX.ino
@@ -1,51 +1,39 @@
 #include <Arduino.h>
 
-int pinServer = 5; // Número do pino usado pelo Arduino para receber dados
-float baudrate = 9600; // 9600 bits por segundo
+int pinServer = 7; 
+float baudrate = 9600;
 int msg;
-bool startBitDetected = false; // Flag para detectar o início de um novo caractere
 
 void setup() {
-  pinMode(pinServer, INPUT); // Configura o pino digital como entrada, pois o servidor está recebendo do cliente
-  Serial.begin(baudrate); // Inicia a porta serial, definindo a taxa de dados de recepção como 9600 bits por segundo
+  pinMode(pinServer, INPUT); 
+  Serial.begin(baudrate);
 }
 
-float timeSkipper(float skipTime = 1, float baudrate = 9600, float T0 = 0) {
-  double clock = 1.0 / (21.0 * pow(10, 6)); // Tempo de um clock (T = 1/frequência), onde a frequência é de 21 MHz
-  double T = 1.0 / baudrate; // Tempo entre cada clock, em segundos
-  int numberOfClocks = floor(T / clock) + 1; // Número de clocks a aguardar, arredondado para o inteiro mais próximo
-  for (int i = 0; i < int(numberOfClocks * skipTime); i++) {
-    asm("NOP"); // Aguarde o tempo especificado
-  }
+float timeSkipper(float skipTime=1, float baudrate=9600, float T0=0){
+  double clock = 1 / (21 * pow(10, 6)); // tempo de 1 pulso do clock (T = 1/frequência)
+  double T = 1 / baudrate; // tempo entre cada pulso do clock, em segundos
+  int numberOfClocks = floor(T / clock) + 1; // número de pulsos do relógio a esperar, arredondado para o inteiro mais próximo
+  for (int i = 0; i < int(numberOfClocks * skipTime); i++){ asm("NOP"); } // aguarde o tempo especificado
 }
 
 void loop() {
-  if (digitalRead(pinServer) == 0) {
-    if (!startBitDetected) {
-      timeSkipper(1.5); // Posiciona o momento para ler bits no meio de cada bit (portanto, o bit de início é ignorado)
-      startBitDetected = true; // Marca o início de um novo caractere
-    } else {
-      int amountOf1s = 0;
-      for (int i = 0; i < 8; i++) {
-        int currentBit = digitalRead(pinServer); // Lê o bit atual
-        timeSkipper(); // Sempre chamado após a leitura de um bit
-        if (currentBit == 1) {
-          amountOf1s++; // Conta a quantidade de bits 1 na mensagem
-        }
-        msg |= (currentBit << i); // Adicione o bit atual à mensagem
-      }
-      int parityBitReceived = digitalRead(pinServer); // Lê o bit de paridade recebido
-      int parityBitCalculated = (amountOf1s % 2); // Calcula o bit de paridade a partir da mensagem enviada
-      if (parityBitReceived == parityBitCalculated) { // Se o bit de paridade recebido for igual ao bit de paridade calculado, a mensagem está correta
-        Serial.print("Caractere recebido: ");
-        Serial.println(char(msg));
-        Serial.println("Bit de paridade está correto");
-      } else { // Se o bit de paridade recebido for diferente do bit de paridade calculado, a mensagem está incorreta
-        Serial.println("ERRO. Bit de paridade não está correto");
-      }
-      msg = 0; // Limpa a variável da mensagem
-      startBitDetected = false; // Marca o fim do caractere atual
-      delay(1500);
+  if (digitalRead(pinServer) == 0){
+    int amountOf1s = 0;
+    timeSkipper(1.5); // posicionando o momento de ler os bits no meio de cada bit (portanto, o bit de início é ignorado)
+    for (int i = 0; i < 8){
+      int currentBit = digitalRead(pinServer); // leia o bit atual
+      timeSkipper(); // sempre chamado após ler um bit
+      if (currentBit == 1){ amountOf1s++; } // conte a quantidade de uns na mensagem
+      msg |= (currentBit << i); // adicione o bit atual à mensagem
     }
+    int parityBitReceived = digitalRead(pinServer); // leia o bit de paridade recebido
+    int parityBitCalculated = (amountOf1s % 2); // calcule o bit de paridade a partir da mensagem enviada
+    if (parityBitReceived == parityBitCalculated){ // se o bit de paridade recebido for igual ao bit de paridade calculado, a mensagem está correta
+      Serial.print("Dados recebidos: ");
+      Serial.println(msg, HEX);
+    } else { // se o bit de paridade recebido for diferente do bit de paridade calculado, a mensagem está incorreta
+      Serial.println("ERRO. O bit de paridade NÃO está correto");
+    }
+    delay(1500);
   }
 }
